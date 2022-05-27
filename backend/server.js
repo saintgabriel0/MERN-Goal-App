57a0a68c5d5f148aa87aac405e42fdb223fc88d5
ChineduG <investorgbriel@gmail.com>
2022-05-27 13:07:46 +0100

    Initials

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3000;
+
+
+
+const app = express()
+
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => 
+console.log(`Server listening on port ${port}`))
